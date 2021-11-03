a9f51edac6dbbdd1af6b2cad02d0790aff146f80
RildoPaul <paulreynaga85@gmail.com>
2021-11-03 19:02:15 +0300

    update

--- trendingbot.py
+++ trendingbot.py
@@ -27,7 +27,7 @@ use_free_proxy = True
 
 thread_limit = 20
 
-wait_range = "10~20"    # Browser random wait second range
+wait_range = "10~20"    # Browser random wait second range 
 
 userAgents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
